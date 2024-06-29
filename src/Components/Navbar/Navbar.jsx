9e75f856ee65ebeab06b168f3ffdda6f6ca5f6e1
snoopyylion <tifase_oluwaseyi@yahoo.com>
2024-06-29 09:46:31 +0100

    css update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt="" /></Link> 
-            <div className="nav-cart-count">{getTotalCartItems}</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
         
     </div>
